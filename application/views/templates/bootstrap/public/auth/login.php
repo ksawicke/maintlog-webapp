30a4e7c7df5f654e07a84ef95a3ce04b73054431
Kevin Sawicke <kevin@rinconmountaintech.com>
2017-09-19 16:46:04 -0700

    Add Komatsu logo and update login method

--- application/views/templates/bootstrap/public/auth/login.php
+++ application/views/templates/bootstrap/public/auth/login.php
@@ -1,3 +1,8 @@
+<p class="text-center" style="margin-top:100px;">
+    <img src="<?php echo $assetDirectoryCustom; ?>img/025_Gloria_blue_nega_r.jpg"><br />
+    <h3><strong>Maintenance Log Application</strong></h3><br />
+</p>
+
 <?php if(array_key_exists('alert_danger', $flashdata)) { ?>
 <div class="alert alert-danger alert-dismissible">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
@@ -15,11 +20,15 @@
 <?php } ?>
 
 <form action="<?php echo base_url(); ?>auth/check" method="post">
-    <label for="username" class="sr-only">Username</label>
-    <input type="text" id="username" name="username" class="form-control" placeholder="Username" required>
+    <p>
+        <label for="username" class="sr-only">Username</label>
+        <input type="text" id="username" name="username" class="form-control" placeholder="Username" required>
+    </p>
     
-    <label for="pin" class="sr-only">PIN</label>
-    <input type="password" id="pin" name="pin" class="form-control" placeholder="PIN" required>
+    <p>
+        <label for="pin" class="sr-only">PIN</label>
+        <input type="password" id="pin" name="pin" class="form-control" placeholder="PIN" required>
+    </p>
     
     <button class="btn btn-lg btn-primary btn-block" type="submit">Log In</button>
 </form>
