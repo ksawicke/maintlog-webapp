b4070ba0a1e3cc640ca39adf98f4428dff711b01
Kevin Sawicke <kevin@rinconmountaintech.com>
2017-09-19 16:01:04 -0700

    Moved CSS file

--- application/views/templates/public_auth.php
+++ application/views/templates/public_auth.php
@@ -13,6 +13,9 @@
 
         <!-- Bootstrap core CSS -->
         <link href="<?php echo $assetDirectory; ?>css/bootstrap.min.css" rel="stylesheet">
+        
+        <!-- Komatsu NA custom CSS -->
+        <link href="<?php echo $assetDirectoryCustom; ?>komatsuna_custom.css" rel="stylesheet">
 
         <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
         <link href="<?php echo $assetDirectory; ?>css/ie10-viewport-bug-workaround.css" rel="stylesheet">
@@ -20,6 +23,10 @@
         <!-- Custom styles for this template -->
         <!--link href="<?php echo $assetDirectory; ?>signin.css" rel="stylesheet"-->
 
+        <link href='https://fonts.googleapis.com/css?family=Libre+Franklin:400,100,100italic,200,200italic,300,300italic,400italic,500,600,500italic,600italic,700,700italic,800,900,800italic,900italic' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=PT+Sans+Caption:400,700' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Patrick+Hand+SC' rel='stylesheet' type='text/css'>
+        
         <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
         <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
         <script src="<?php echo $assetDirectory; ?>js/ie-emulation-modes-warning.js"></script>
