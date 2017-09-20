eae2752b6ccdd2a5c8f525df78ecd4e948e8b631
Kevin Sawicke <kevin@rinconmountaintech.com>
2017-09-20 14:15:02 -0700

    Build of log entry form

--- application/views/templates/bootstrap/authenticated/app/log_entry.php
+++ application/views/templates/bootstrap/authenticated/app/log_entry.php
@@ -1,47 +1,74 @@
 <form id="serviceLog" action="#">
+    
     <div class="group">
-        <label for="date_entered">Date</label>
-        <input type="text" id="date_entered" name="date_entered" />
+        <div class="form-group col-xs-8 col-md-8">
+            <label for="date_entered" class="control-label">Date Entered</label>
+            <input type="text" id="date_entered" name="date_entered" class="form-control" value="<?php echo date("m/d/Y"); ?>">
+        </div>
     </div>
+    
     <div class="group">
-        <label for="entered_by">Entered By</label>
-        <select id="entered_by" name="entered_by">
-            <option value="">Select one:</option>
-            <option value="124566">Doe, John</option>
-            <option value="124512">Johnson, Neil</option>
-            <option value="622141">Smith, Joe</option>
-        </select>
+        <div class="form-group col-xs-8 col-md-8">
+            <label for="entered_by" class="control-label">Entered By</label>
+            <select id="entered_by" name="entered_by" class="form-control">
+                <option value="">Select one:</option>
+                <option value="124566">Doe, John</option>
+                <option value="124512">Johnson, Neil</option>
+                <option value="622141">Smith, Joe</option>
+            </select>
         
-        <label for="serviced_by">Serviced By</label>
-        <select id="serviced_by" name="serviced_by" multiple>
-            <option value="124566">Doe, John</option>
-            <option value="124512">Johnson, Neil</option>
-            <option value="622141">Smith, Joe</option>
-            <option value="622141">Xavier, Jose</option>
-        </select>
+            <label for="serviced_by" class="control-label">Serviced By</label>
+            <select id="serviced_by" name="serviced_by" class="form-control" multiple>
+                <option value="124566">Doe, John</option>
+                <option value="124512">Johnson, Neil</option>
+                <option value="622141">Smith, Joe</option>
+                <option value="622141">Xavier, Jose</option>
+            </select>
+        </div>
+    </div>
+    
+    <div class="group">
+        <div class="form-group col-xs-8 col-md-8">
+            <label for="equipment_type" class="control-label">Equipment Type</label>
+            <select id="equipment_type" name="equipment_type" class="form-control">
+                <option value="">Select one:</option>
+                <option value="22252525">Loader</option>
+                <option value="2352">Forklift</option>
+                <option value="43435352">Light Vehicle</option>
+                <option value="253788">Other</option>
+            </select>
+        
+            <label for="equipment" class="control-label">Equipment</label>
+            <select id="equipment" name="equipment" class="form-control">
+                <option value="1000">Unit 1000 | Manufacturer Model 100</option>
+                <option value="1002">Unit 1002 | Manufacturer Model 600</option>
+                <option value="1004">Unit 1004 | Manufacturer Model 400</option>
+                <option value="1006">Unit 1006 | Manufacturer Model 300</option>
+            </select>
+        </div>
+    </div>
+    
+    <div class="group">
+        <label for="value4" class="control-label">Value 4</label>
+        <input type="text" id="value4" name="value4" class="form-control" />
     </div>
     <div class="group">
-        <label for="value3">Value 3</label>
-        <input type="text" id="value3" name="value3" />
+        <label for="value5" class="control-label">Value 5</label>
+        <input type="text" id="value5" name="value5" class="form-control" />
     </div>
     <div class="group">
-        <label for="value4">Value 4</label>
-        <input type="text" id="value4" name="value4" />
+        <label for="value6" class="control-label">Value 6</label>
+        <input type="text" id="value6" name="value6" class="form-control" />
     </div>
     <div class="group">
-        <label for="value5">Value 5</label>
-        <input type="text" id="value5" name="value5" />
+        <label for="value7" class="control-label">Value 7 (last one)</label>
+        <input type="text" id="value7" name="value7" class="form-control" />
     </div>
-    <div class="group">
-        <label for="value6">Value 6</label>
-        <input type="text" id="value6" name="value6" />
-    </div>
-    <div class="group">
-        <label for="value7">Value 7 (last one)</label>
-        <input type="text" id="value7" name="value7" />
-    </div>
+    <br style="clear:both;" />
     <div>
-        <button id="btnNext" type="submit">Next</button>
+        <div class="form-group col-xs-4 col-md-4">
+            <button id="btnNext" type="submit">Next</button>
+        </div>
     </div>
 </form>
 
