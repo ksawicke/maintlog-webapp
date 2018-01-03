428309c5de7092c16b2b2cba49febd63ae844bba
Kevin Sawicke <kevin@rinconmountaintech.com>
2018-01-03 14:24:24 -0700

    Add flexibility to search on Maintenance Log Reminders Report

--- application/views/templates/bootstrap/authenticated/app/reporting/maintenance_log_reminders.php
+++ application/views/templates/bootstrap/authenticated/app/reporting/maintenance_log_reminders.php
@@ -14,34 +14,65 @@
             <th>SMR Due</th>
         </tr>
     </thead>
-    <tbody>
-        <?php foreach($maintenance_log_reminders as $ctr => $reminder) { ?>
+    <tfoot>
         <tr>
-            <td><?php echo date('m/d/Y', strtotime($reminder['date_entered'])); ?></td>
-            <td><?php echo $reminder['current_smr']; ?></td>
-            <td><?php echo $reminder['manufacturer_name']; ?></td>
-            <td><?php echo $reminder['model_number']; ?></td>
-            <td><?php echo $reminder['unit_number']; ?></td>
-            <td><?php echo $reminder['notes']; ?></td>
-            <td><?php echo $reminder['due_units']; ?></td>
+            <th>Last Entry Date</th>
+            <th>Last SMR</th>
+            <th>Manufacturer Name</th>
+            <th>Model Name</th>
+            <th>Unit Number</th>
+            <th>Notes</th>
+            <th>SMR Due</th>
         </tr>
+    </tfoot>
+    <tbody>
+        <?php foreach ($maintenance_log_reminders as $ctr => $reminder) { ?>
+            <tr>
+                <td><?php echo date('m/d/Y', strtotime($reminder['date_entered'])); ?></td>
+                <td><?php echo $reminder['current_smr']; ?></td>
+                <td><?php echo $reminder['manufacturer_name']; ?></td>
+                <td><?php echo $reminder['model_number']; ?></td>
+                <td><?php echo $reminder['unit_number']; ?></td>
+                <td><?php echo $reminder['notes']; ?></td>
+                <td><?php echo $reminder['due_units']; ?></td>
+            </tr>
         <?php } ?>
     </tbody>
 </table>
 
 <script>
-  $(document).ready(function() {
-    $('#maintenanceLogRemindersReport').DataTable({
-        "columns": [
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null
+    $(document).ready(function () {
+        $('#maintenanceLogRemindersReport').DataTable({
+            initComplete: function () {
+                this.api().columns().every(function () {
+                    var column = this;
+                    var select = $('<select><option value=""></option></select>')
+                            .appendTo($(column.footer()).empty())
+                            .on('change', function () {
+                                var val = $.fn.dataTable.util.escapeRegex(
+                                        $(this).val()
+                                        );
+
+                                column
+                                        .search(val ? '^' + val + '$' : '', true, false)
+                                        .draw();
+                            });
+
+                    column.data().unique().sort().each(function (d, j) {
+                        select.append('<option value="' + d + '">' + d + '</option>')
+                    });
+                });
+            },
+            "columns": [
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
 //            {"width": "80px", "orderable": false}
-        ]
+            ]
+        });
     });
-  });
 </script>
