8215879082a297348687f6079c1ec75d29ff3066
Kevin Sawicke <kevin@rinconmountaintech.com>
2018-02-22 07:28:48 -0700

    Update report navigation style

--- application/views/templates/bootstrap/authenticated/app/reporting/reports_navigation.php
+++ application/views/templates/bootstrap/authenticated/app/reporting/reports_navigation.php
@@ -3,13 +3,13 @@
 </a>
 
 <a href="<?php echo base_url('index.php/reporting/output/screen/service_logs'); ?>">
-    <button type="button" class="btn btn-<?php echo ($report_type=='service_logs' || $report_type=='service_log_detail' ? 'primary' : 'default'); ?>">Service Logs</button>
+    <button type="button" class="btn btn-<?php echo ($report_type=='service_logs' || $report_type=='service_log_detail' ? 'primary' : 'highlight'); ?>">Service Logs</button>
 </a>
 
 <a href="<?php echo base_url('index.php/reporting/output/screen/pmservice_reminders'); ?>">
-    <button type="button" class="btn btn-<?php echo ($report_type=='pmservice_reminders' ? 'primary' : 'default'); ?>">PM Service Reminders</button>
+    <button type="button" class="btn btn-<?php echo ($report_type=='pmservice_reminders' ? 'primary' : 'highlight'); ?>">PM Service Reminders</button>
 </a>
 
 <a href="<?php echo base_url('index.php/reporting/output/screen/equipment_list'); ?>">
-    <button type="button" class="btn btn-<?php echo ($report_type=='equipment_list' ? 'primary' : 'default'); ?>">Equipment List</button>
+    <button type="button" class="btn btn-<?php echo ($report_type=='equipment_list' ? 'primary' : 'highlight'); ?>">Equipment List</button>
 </a>
