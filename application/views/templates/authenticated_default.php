2bc905007b23ba6ee7f2d8fbd234752361fcb51a
Kevin Sawicke <kevin@rinconmountaintech.com>
2018-03-01 09:50:14 -0700

    Remove support for IE8 elements

--- application/views/templates/authenticated_default.php
+++ application/views/templates/authenticated_default.php
@@ -37,23 +37,16 @@
 	<link href="<?php echo $assetDirectoryCustom; ?>css/parsley.css" rel="stylesheet" type="text/css">
 	<link href="<?php echo $assetDirectoryCustom; ?>css/komatsuna_custom.css" rel="stylesheet" type="text/css">
 
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!--[if lt IE 9]>
-	<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-
-	<script src="<?php echo $assetDirectoryCustom; ?>js/jquery.min.js"></script>
-	<script src="<?php echo $assetDirectoryCustom; ?>js/jquery-ui.min.js"></script>
-	<script src="<?php echo $assetDirectoryCustom; ?>js/jquery.ui.touch-punch.min.js"></script>
-	<script src="<?php echo $assetDirectoryCustom; ?>js/bootstrap-datepicker.min.js"></script>
-
-	<script type="text/javascript" src="<?php echo $assetDirectoryCustom; ?>js/jBox.min.js"></script>
-	<script type="text/javascript" src="<?php echo $assetDirectoryCustom; ?>js/datatables.min.js"></script>
-	<script type="text/javascript" src="<?php echo $assetDirectoryCustom; ?>js/dataTables.responsive.min.js"></script>
-	<script type="text/javascript" src="<?php echo $assetDirectoryCustom; ?>js/parsley.min.js"></script>
-	<script type="text/javascript" src="<?php echo $assetDirectoryCustom; ?>js/parsley.comparison.js"></script>
-	<script type="text/javascript" src="<?php echo $assetDirectoryCustom; ?>js/moment.min.js"></script>
+	<script src="<?php echo $assetDirectoryCustom; ?>js/jquery.min.js" type="text/javascript"></script>
+	<script src="<?php echo $assetDirectoryCustom; ?>js/jquery-ui.min.js" type="text/javascript"></script>
+	<script src="<?php echo $assetDirectoryCustom; ?>js/jquery.ui.touch-punch.min.js" type="text/javascript"></script>
+	<script src="<?php echo $assetDirectoryCustom; ?>js/bootstrap-datepicker.min.js" type="text/javascript"></script>
+	<script src="<?php echo $assetDirectoryCustom; ?>js/jBox.min.js" type="text/javascript"></script>
+	<script src="<?php echo $assetDirectoryCustom; ?>js/datatables.min.js" type="text/javascript"></script>
+	<script src="<?php echo $assetDirectoryCustom; ?>js/dataTables.responsive.min.js" type="text/javascript"></script>
+	<script src="<?php echo $assetDirectoryCustom; ?>js/parsley.min.js" type="text/javascript"></script>
+	<script src="<?php echo $assetDirectoryCustom; ?>js/parsley.comparison.js" type="text/javascript"></script>
+	<script src="<?php echo $assetDirectoryCustom; ?>js/moment.min.js" type="text/javascript"></script>
 </head>
 
 <body>
