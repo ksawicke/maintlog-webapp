3964d215a69e71d69a19a6ce07fcc0260381d816
Kevin Sawicke <kevin@rinconmountaintech.com>
2018-03-01 09:47:49 -0700

    Adjustment to allow drag items to checklists via touch to support iPad and iPhone

--- application/views/templates/bootstrap/authenticated/app/checklists/add.php
+++ application/views/templates/bootstrap/authenticated/app/checklists/add.php
@@ -33,27 +33,24 @@
 
 	<div class="row">
 		<div class="col-sm-4 col-md-4 col-lg-4">
-			<br />
-			<strong>Pre-Start</strong>
-			<ul id="preStartSortableItemList" class="preStart connectedSortable">
+			<label for="preStartSortableItemList" class="control-label lb-lg">Pre-Start</label>
+			<ul id="preStartSortableItemList" class="preStartSelected connectedSortable">
 			</ul>
 		</div>
 		<div class="col-sm-4 col-sm-4 col-lg-4">
-			<br />
-			<strong>Available Items</strong>
+			<label for="availableItemSortableItemList" class="control-label lb-lg">Available Items</label>
 			<ul id="availableItemSortableItemList" class="connectedSortable">
 			</ul>
 		</div>
 		<div class="col-sm-4 col-md-4 col-lg-4">
-			<br />
-			<strong>Post-Start</strong>
-			<ul id="postStartSortableItemList" class="postStart connectedSortable">
+			<label for="postStartSortableItemList" class="control-label lb-lg">Post-Start</label>
+			<ul id="postStartSortableItemList" class="postStartSelected connectedSortable">
 			</ul>
 		</div>
 	</div>
 
 	<input type="hidden" id="checklist_id" name="checklist_id" value="<?php echo $checklist_id; ?>">
-	<input type="hidden" id="checklist_json" name="checklist_json" value="<?php echo $checklist_json; ?>">
+	<input type="hidden" id="checklist_json" name="checklist_json" value='<?php echo $checklist_json; ?>'>
 
 	<button id="btnSubmit" type="submit" class="btn btn-lg btn-primary" disabled>Submit</button>
 
@@ -63,42 +60,31 @@
 
 	$(function () {
 
-		$("#preStartSortableItemList, #availableItemSortableItemList, #postStartSortableItemList").sortable({
-			connectWith: ".connectedSortable",
-			update: function( event, ui ) {
-				// console.log("Something changed...");
-				// console.log(event);
-				// console.log(ui);
-
-				// var sorted = $( ".preStart" ).sortable( "serialize", { key: "sort" } );
-				// var sortedIds = $( ".preStart" ).sortable( "toArray" );
-                //
-				// var sorted2 = $( ".postStart" ).sortable( "serialize", { key: "sort" } );
-				// var sortedIds2 = $( ".postStart" ).sortable( "toArray" );
-
-				// console.log(sorted);
-				// console.log(sortedIds);
-                //
-				// console.log(sorted2);
-				// console.log(sortedIds2);
-
-				var preStartData = $(".preStart").sortable( "toArray" );
-				var postStartData = $(".postStart").sortable( "toArray" );
-				var checklist_json = JSON.stringify({preStartData: preStartData, postStartData: postStartData});
-
+		function updateChecklistJson(object, checklist_json) {
+			if(object[0].id!='availableItemSortableItemList') {
 				$("#checklist_json").val(checklist_json);
 				console.log($("#checklist_json").val());
 			}
-		}).disableSelection();
+		}
 
-		function loadEmUp() {
+		function populateAvailableItems() {
 			for(i=1; i <= 10; i++) {
 				$("#availableItemSortableItemList").append('<li class="ui-state-highlight" id="' + i + '">Item ' + i + '</li>');
 			}
 		}
 
-		loadEmUp();
+		$("#preStartSortableItemList, #availableItemSortableItemList, #postStartSortableItemList").sortable({
+			connectWith: ".connectedSortable",
+			update: function( event, ui ) {
+				var preStartData = $(".preStartSelected").sortable( "toArray" );
+				var postStartData = $(".postStartSelected").sortable( "toArray" );
+				var checklist_json = JSON.stringify({preStartData: preStartData, postStartData: postStartData});
 
+				updateChecklistJson($(this), checklist_json);
+			}
+		}).disableSelection();
+
+		populateAvailableItems();
 	});
 
 </script>
