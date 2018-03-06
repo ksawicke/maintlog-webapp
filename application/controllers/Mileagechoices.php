fdc5308d78dd2aa821268d1b2d16cd49878c1a3f
Kevin Sawicke <kevin@rinconmountaintech.com>
2018-03-06 11:40:13 -0700

    Update to version 2.3

--- application/controllers/Mileagechoices.php
+++ application/controllers/Mileagechoices.php
@@ -10,7 +10,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 *
 * @package  Maintenance Log Application
 * @author   Kevin Sawicke <kevin@rinconmountaintech.com>
-* @version  $Revision: Tag v2.2.1 $
+* @version  $Revision: Tag v2.3 $
 * @access   public
 */
 class Mileagechoices extends MY_Controller {
--- application/controllers/Reporting.php
+++ application/controllers/Reporting.php
@@ -13,7 +13,7 @@ use PhpOffice\PhpSpreadsheet\Spreadsheet;
 *
 * @package  Maintenance Log Application
 * @author   Kevin Sawicke <kevin@rinconmountaintech.com>
-* @version  $Revision: Tag v2.2.1 $
+* @version  $Revision: Tag v2.3 $
 * @access   public
 */
 class Reporting extends MY_Controller {
