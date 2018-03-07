4e78dae0571bfe285a5687ced60f385fcd512e05
Kevin Sawicke <kevin@rinconmountaintech.com>
2018-03-06 17:38:04 -0700

    Update datepicker style

--- application/views/templates/bootstrap/authenticated/app/inspection_entry.php
+++ application/views/templates/bootstrap/authenticated/app/inspection_entry.php
@@ -3,7 +3,7 @@
 		<label for="date_entered" class="control-label lb-lg">Date Entered</label>
 		<div class="input-group date">
 			<div class="input-group-addon">
-				<i class="fa fa-calendar"></i>
+				<i class="far fa-calendar-alt fa-2x"></i>
 			</div>
 			<input id="date_entered"
 				   name="date_entered"
