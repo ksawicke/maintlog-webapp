ed4bd744557bbc9e879afe44b9130025ee601422
Kevin Sawicke <kevin@rinconmountaintech.com>
2018-05-18 09:07:09 -0700

    Adjusted the subclass prefix as it interfered with the App post login Verified that API endpoint to upload Inspections still works as well

--- application/config/config.php
+++ application/config/config.php
@@ -127,7 +127,7 @@ $config['enable_hooks'] = TRUE;
 | https://codeigniter.com/user_guide/general/creating_libraries.html
 |
 */
-$config['subclass_prefix'] = 'REST_';
+$config['subclass_prefix'] = 'MY_';
 
 /*
 |--------------------------------------------------------------------------
