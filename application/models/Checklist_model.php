4a1c732a7154eed921afc9609c0ef77f0fa17518
Kevin Sawicke <kevin@rinconmountaintech.com>
2018-06-06 15:58:47 -0700

    Update App

--- application/models/Checklist_model.php
+++ application/models/Checklist_model.php
@@ -45,24 +45,24 @@ class Checklist_model extends CI_Model {
 	public function store($post) {
 		$now = date('Y-m-d h:i:s');
 
-		$checklist = ($post['checklist_id']==0 ? R::dispense('checklist') : R::load('checklist', $post['checklist_id']));
+		$checklist = ($post['id']==0 ? R::dispense('checklist') : R::load('checklist', $post['id']));
 		
 		$checklist->equipmenttype_id = $post['equipmenttype_id'];
 		$checklist->checklist_json = $post['checklist_json'];
 
-		if($post['checklist_id']==0) {
+		if($post['id']==0) {
 			$checklist->created = $now;
 			$checklist->created_by = $_SESSION['user_id'];
 		} else {
 			$checklist->modified = $now;
 			$checklist->modified_by = $_SESSION['user_id'];
 		}
-
-		echo '<pre>';
-		var_dump($post);
-		var_dump($checklist);
-		echo '</pre>';
-		exit();
+//
+//		echo '<pre>';
+//		var_dump($post);
+//		var_dump($checklist);
+//		echo '</pre>';
+//		exit();
 		
 		R::store($checklist);
 	}
